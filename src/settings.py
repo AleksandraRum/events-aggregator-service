c18d5392e5ad23910c54b4ae027b81ef21bf21c4
Aleksandra Rumiantseva <alexrumyantceva@gmail.com>
2026-02-20 13:40:29 +0300

    add hosts

--- src/settings.py
+++ src/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wjr!bgykv!=ka4ba36+po)hg+u*b!o&fnxnu=9_)x#l0+kc$yu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
